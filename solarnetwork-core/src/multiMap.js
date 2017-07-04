b6bea0bcca7bff9141cf0431d0de195f248ebeab
Matt Magoffin <matt@solarnetwork.net>
2017-07-05 10:08:11 +1200

    Add MultiMap class, to support HttpHeaders.

---
+++ solarnetwork-core/src/multiMap.js
@@ -0,0 +1,76 @@
+/**
+ * A case-insensitive multi-value map object.
+ *
+ * @class
+ */
+class MultiMap {
+	constructor() {
+		this.mappings = {}; // map of lower-case header names to {name:X, val:[]} values
+		this.mappingNames = []; // to keep insertion order
+	}
+
+	add(key, value) {
+		return addValue(this, key, value);
+	}
+
+	put(key, value) {
+		return addValue(this, key, value, true);
+	}
+
+	putAll(values) {
+		var key;
+		for ( key in values ) {
+			addValue(this, key, values[key], true);
+		}
+		return this;
+	}
+
+	value(key) {
+		const keyLc = key.toLowerCase();
+		const mapping = this.mappings[keyLc];
+		return (mapping ? mapping.val : undefined);
+	}
+
+	firstValue(key) {
+		const values = this.value(key);
+		return (values && values.length > 0 ? values[0] : undefined);
+	}
+
+	clear() {
+		this.mappingNames.length = 0;
+		this.mappings = {};
+		return this;
+	}
+
+	size() {
+		return this.mappingNames.length;
+	}
+
+	isEmpty() {
+		return this.size() < 1;
+	}
+}
+
+function addValue(map, key, value, replace) {
+	const keyLc = key.toLowerCase();
+	var mapping = map.mappings[keyLc];
+	if ( !mapping ) {
+		mapping = {key:key, val:[]};
+		map.mappings[keyLc] = mapping;
+		map.mappingNames.push(keyLc);
+	}
+	if ( replace ) {
+		mapping.val.length = 0;
+	}
+	if ( Array.isArray(value) ) {
+		const len = value.length;
+		for ( let i = 0; i < len; i += 1 ) {
+			mapping.val.push(value[i]);
+		}
+	} else {
+		mapping.val.push(value);
+	}
+	return map;
+}
+
+export default MultiMap;
---
+++ solarnetwork-core/test/multiMapTests.js
@@ -0,0 +1,103 @@
+'use strict';
+
+import test from 'ava';
+
+import MultiMap from 'multiMap';
+
+test('core:multiMap:create', t => {
+	const map = new MultiMap();
+	t.truthy(map);
+});
+
+test('core:multiMap:put', t => {
+	const map = new MultiMap();
+	t.is(map.put('foo', 'bar'), map);
+	t.deepEqual(map.value('foo'), ['bar']);
+});
+
+test('core:multiMap:putArray', t => {
+	const map = new MultiMap();
+	t.is(map.put('foo', ['bar', 'bam']), map);
+	t.deepEqual(map.value('foo'), ['bar', 'bam']);
+});
+
+test('core:multiMap:firstValue:undefined', t => {
+	const map = new MultiMap();
+	t.true(map.firstValue('foo') === undefined);
+});
+
+test('core:multiMap:firstValue:single', t => {
+	const map = new MultiMap();
+	map.put('foo', 'bar');
+	t.is(map.firstValue('foo'), 'bar');
+});
+
+test('core:multiMap:firstValue:array', t => {
+	const map = new MultiMap();
+	map.put('foo', ['bim', 'bam']);
+	t.is(map.firstValue('foo'), 'bim');
+});
+
+test('core:multiMap:putAll', t => {
+	const map = new MultiMap();
+	t.is(map.putAll({foo:'bar', bim:'bam'}), map);
+	t.deepEqual(map.value('foo'), ['bar']);
+	t.deepEqual(map.value('bim'), ['bam']);
+});
+
+test('core:multiMap:putAllWithArray', t => {
+	const map = new MultiMap();
+	t.is(map.putAll({foo:'bar', bim:['bam', 'bum']}), map);
+	t.deepEqual(map.value('foo'), ['bar']);
+	t.deepEqual(map.value('bim'), ['bam', 'bum']);
+});
+
+test('core:multiMap:size:empty', t => {
+	const map = new MultiMap();
+	t.is(map.size(), 0);
+});
+
+test('core:multiMap:size:single', t => {
+	const map = new MultiMap();
+	map.put('foo', 'bar');
+	t.is(map.size(), 1);
+});
+
+test('core:multiMap:size:multi', t => {
+	const map = new MultiMap();
+	map.putAll({foo:'bar', bim:['bam', 'bum']});
+	t.is(map.size(), 2);
+});
+
+test('core:multiMap:clear:empty', t => {
+	const map = new MultiMap();
+	t.is(map.clear(), map);
+	t.is(map.size(), 0);
+});
+
+test('core:multiMap:clear:multi', t => {
+	const map = new MultiMap();
+	map.putAll({foo:'bar', bim:['bam', 'bum']});
+	t.is(map.clear(), map);
+	t.is(map.size(), 0);
+});
+
+test('core:multiMap:isEmpty:empty', t => {
+	const map = new MultiMap();
+	t.true(map.isEmpty());
+});
+
+test('core:multiMap:isEmpty:not', t => {
+	const map = new MultiMap();
+	map.put('foo', 'bar');
+	t.false(map.isEmpty());
+});
+
+test('core:multiMap:isEmpty:afterClear', t => {
+	const map = new MultiMap();
+	map.put('foo', 'bar');
+	t.false(map.isEmpty());
+	map.clear();
+	t.true(map.isEmpty());
+});
+
