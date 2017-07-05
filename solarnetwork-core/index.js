6a959ffeff7b061017ebf322c3c6713a1cbdff21
Matt Magoffin <matt@solarnetwork.net>
2017-07-06 10:04:07 +1200

    Add epxorts from format/date.

--- solarnetwork-core/index.js
+++ solarnetwork-core/index.js
@@ -1,7 +1,8 @@
-export {default as configuration} 	from './src/configuration';
-export {default as multiMap} 		from './src/multiMap';
-export {default as netAuthV2} 		from './src/net/authV2';
-export {default as netEnvironment} 	from './src/net/environment';
-export {default as netHttpHeaders} 	from './src/net/httpHeaders';
-export {default as netUrlHelper} 	from './src/net/urlHelper';
-export {default as netUrlQuery} 	from './src/net/urlQuery';
+export {default as configuration}   from './src/configuration';
+export {default as multiMap}        from './src/multiMap';
+export *                            from './src/format/date';
+export {default as netAuthV2}       from './src/net/authV2';
+export {default as netEnvironment}  from './src/net/environment';
+export {default as netHttpHeaders}  from './src/net/httpHeaders';
+export {default as netUrlHelper}    from './src/net/urlHelper';
+export {default as netUrlQuery}     from './src/net/urlQuery';
