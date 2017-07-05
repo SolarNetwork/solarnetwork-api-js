486221fb3cfbcd78517958c99dbecdb3de6bf22e
Matt Magoffin <matt@solarnetwork.net>
2017-07-05 12:00:48 +1200

    Rename tests for consistency.

--- solarnetwork-core/test/net/urlQueryTests.js
+++ solarnetwork-core/test/net/urlQueryTests.js
@@ -5,39 +5,39 @@ import test from 'ava';
 import { urlQueryParse, urlQueryEncode } from 'net/urlQuery';
 import urlQuery from 'net/urlQuery';
 
-test('core:urlQuery:encode:simple', t => {
+test('core:net:urlQuery:encode:simple', t => {
 	t.is(urlQueryEncode({foo:'bar'}), 'foo=bar');
 });
 
-test('core:urlQuery:encode:array', t => {
+test('core:net:urlQuery:encode:array', t => {
 	t.is(urlQueryEncode({foo:['one','two']}), 'foo=one&foo=two');
 });
 
-test('core:urlQuery:encode:multi', t => {
+test('core:net:urlQuery:encode:multi', t => {
 	t.is(urlQueryEncode({foo:'bar', bim:'bam', life:42}), 'foo=bar&bim=bam&life=42');
 });
 
-test('core:urlQuery:encode:escaped', t => {
+test('core:net:urlQuery:encode:escaped', t => {
 	t.is(urlQueryEncode({foo:'sn == crazy & cool!'}), 'foo=sn%20%3D%3D%20crazy%20%26%20cool!');
 });
 
-test('core:urlQuery:parse:simple', t => {
+test('core:net:urlQuery:parse:simple', t => {
 	t.deepEqual(urlQueryParse('foo=bar'), {foo:'bar'});
 });
 
-test('core:urlQuery:parse:array', t => {
+test('core:net:urlQuery:parse:array', t => {
 	t.deepEqual(urlQueryParse('foo=one&foo=two'), {foo:['one','two']});
 });
 
-test('core:urlQuery:parse:multi', t => {
+test('core:net:urlQuery:parse:multi', t => {
 	t.deepEqual(urlQueryParse('foo=bar&bim=bam&life=42'), {foo:'bar', bim:'bam', life:'42'});
 });
 
-test('core:urlQuery:parse:escaped', t => {
+test('core:net:urlQuery:parse:escaped', t => {
 	t.deepEqual(urlQueryParse('foo=sn%20%3D%3D%20crazy%20%26%20cool!'), {foo:'sn == crazy & cool!'});
 });
 
-test('core:urlQuery:defaultExport', t => {
+test('core:net:urlQuery:defaultExport', t => {
 	t.is(urlQuery.urlQueryParse, urlQueryParse);
 	t.is(urlQuery.urlQueryEncode, urlQueryEncode);
 });
