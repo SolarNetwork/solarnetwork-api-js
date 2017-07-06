57ac535ad773a57144856aabce02cb3ca5f10d8f
Matt Magoffin <matt@solarnetwork.net>
2017-07-06 13:08:53 +1200

    Treat d3 as an external resource for Rollup.

--- solarnetwork-core/rollup.config.js
+++ solarnetwork-core/rollup.config.js
@@ -10,9 +10,10 @@ const includePathOptions = {
 
 export default {
   external: id => {
-    return /crypto-js/.test(id);
+    return /(crypto-js|d3-)/.test(id);
   },
   globals: {
+    'd3-time-format': 'd3',
     'crypto-js/enc-hex': 'CryptoJS.Hex',
     'crypto-js/hmac-sha256': 'CryptoJS.HmacSHA256',
     'crypto-js/sha256': 'CryptoJS.SHA256',
