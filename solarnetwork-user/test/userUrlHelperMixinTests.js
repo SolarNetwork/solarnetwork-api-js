96caf1b4960be4b986ac6e39e66454c4e4b3e5c4
Matt Magoffin <matt@solarnetwork.net>
2017-07-06 18:58:47 +1200

    Add tests for double mixin of UserUrlHelperMixin and NodeUrlHelperMixin.

--- solarnetwork-user/test/userUrlHelperMixinTests.js
+++ solarnetwork-user/test/userUrlHelperMixinTests.js
@@ -3,6 +3,7 @@
 import test from 'ava';
 
 import UrlHelper from 'net/urlHelper';
+import NodeUrlHelperMixin from 'net/nodeUrlHelperMixin'
 import UserUrlHelperMixin from 'userUrlHelperMixin'
 
 class UserUrlHelper extends UserUrlHelperMixin(UrlHelper) {
@@ -22,4 +23,19 @@ test('user:userUrlHelperMixin:baseUrl', t => {
 test('user:userUrlHelperMixin:viewNodesUrl', t => {
 	const helper = new UserUrlHelper();
 	t.is(helper.viewNodesUrl(), 'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes');
+});
+
+class UserNodeUrlHelper extends UserUrlHelperMixin(NodeUrlHelperMixin(UrlHelper)) {
+	
+}
+
+test('user:userNodeUrlHelperMixin:baseUrl', t => {
+	const helper = new UserNodeUrlHelper();
+	t.is(helper.baseUrl(), 'https://data.solarnetwork.net/solaruser/api/v1/sec');
+});
+
+test('user:userNodeUrlHelperMixin:nodeId', t => {
+	const helper = new UserNodeUrlHelper();
+	helper.nodeId = 123;
+	t.deepEqual(helper.nodeIds, [123]);
 });
