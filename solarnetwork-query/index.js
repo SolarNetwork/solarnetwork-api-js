b13c311659dc72c9db7eb061d2817432faaaa147
Matt Magoffin <matt@solarnetwork.net>
2017-07-07 15:59:29 +1200

    Add module imports.

--- solarnetwork-query/index.js
+++ solarnetwork-query/index.js
@@ -1,1 +1,10 @@
-export { default as DatumAggregation } from './src/datum/aggregation';
+export { default as Aggregation } from './src/aggregation';
+export { default as Pagination } from './src/pagination';
+export { default as QueryUrlHelperMixin,
+	DefaultSolarQueryPath,
+	SolarQueryPathKey,
+	SolarQueryApiPathV1,
+	SolarQueryPublicPathKey } from './src/queryUrlHelperMixin';
+export { default as SortDescriptor } from './src/sortDescriptor';
+export { default as NodeDatumUrlHelperMixin,
+	NodeDatumUrlHelper } from './src/datum/nodeDatumUrlHelperMixin';
