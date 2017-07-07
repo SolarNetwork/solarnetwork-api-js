5d5925186e3ba23c45a7ff5e13a1eca3da51effc
Matt Magoffin <matt@solarnetwork.net>
2017-07-07 15:59:16 +1200

    Remove whitespace.

--- solarnetwork-query/src/datum/nodeDatumUrlHelperMixin.js
+++ solarnetwork-query/src/datum/nodeDatumUrlHelperMixin.js
@@ -4,10 +4,10 @@ import QueryUrlHelperMixin from '../queryUrlHelperMixin';
 
 /**
  * A mixin class that adds SolarNode datum query support to {@code UrlHelper}.
- * 
+ *
  * This mixin extends the {@link QueryUrlHelperMixin} and {@link NodeUrlHelperMixin} mixins.
- * 
- * @param {UrlHelper} superclass the UrlHelper class to mix onto 
+ *
+ * @param {UrlHelper} superclass the UrlHelper class to mix onto
  * @preserve
  */
 const NodeDatumUrlHelperMixin = (superclass) => class extends QueryUrlHelperMixin(NodeUrlHelperMixin(superclass)) {
@@ -15,9 +15,9 @@ const NodeDatumUrlHelperMixin = (superclass) => class extends QueryUrlHelperMixi
 	/**
 	 * Generate a URL for the "reportable interval" for a node, optionally limited to a specific set of source IDs.
 	 *
-     * If no source IDs are provided, then the reportable interval query will return an interval for 
+     * If no source IDs are provided, then the reportable interval query will return an interval for
      * all available sources.
-     * 
+     *
 	 * @param {string[]} sourceIds an array of source IDs to limit query to; if not provided the <code>sourceIds</code> property of this class will be used
 	 * @param {number} [nodeId] a specific node ID to use; if not provided the <code>nodeId</code> property of this class will be used
 	 * @returns {string} the URL
@@ -36,7 +36,7 @@ const NodeDatumUrlHelperMixin = (superclass) => class extends QueryUrlHelperMixi
 
 /**
  * A concrete {@link UrlHelper} with the {@link NodeDatumUrlHelperMixin}.
- * 
+ *
  * @class
  * @preserve
  */
