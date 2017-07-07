3b6be3247ee2c04edaec11a236a76bba4c621b30
Matt Magoffin <matt@solarnetwork.net>
2017-07-07 20:48:37 +1200

    Add package to join all sub-projects together.

---
+++ index.js
@@ -0,0 +1,4 @@
+export {version} from "./build/package";
+export * from "./solarnetwork-core/index.js";
+export * from "./solarnetwork-query/index.js";
+export * from "./solarnetwork-user/index.js";
---
+++ rollup.config.js
@@ -0,0 +1,39 @@
+import babel from 'rollup-plugin-babel';
+import includePaths from 'rollup-plugin-includepaths';
+
+const includePathOptions = {
+  include: {},
+  paths: [
+  	'solarnetwork-core/src',
+  	'solarnetwork-query/src',
+  	'solarnetwork-user/src',
+  ],
+  external: [],
+  extensions: ['.js']
+};
+
+export default {
+  external: id => {
+    return /(crypto-js|d3-)/.test(id);
+  },
+  globals: {
+    'd3-time-format': 'd3',
+    'crypto-js/enc-hex': 'CryptoJS.Hex',
+    'crypto-js/hmac-sha256': 'CryptoJS.HmacSHA256',
+    'crypto-js/sha256': 'CryptoJS.SHA256',
+  },
+  plugins: [
+  	includePaths(includePathOptions),
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      plugins: ['external-helpers'],
+      presets: [
+        ["latest", {
+          "es2015": {
+            "modules": false
+          }
+        }]
+      ]
+    })]
+};
