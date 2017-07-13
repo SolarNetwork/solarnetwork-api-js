bc38b6aff89a01bbcd8913108e7121a436c7723c
Matt Magoffin <matt@solarnetwork.net>
2017-07-13 12:02:17 +1200

    Move Pagination and SortDescriptor into core package (will share with user package).

--- solarnetwork-core/index.js
+++ solarnetwork-core/index.js
@@ -1,10 +1,12 @@
-export {default as Configuration}   from './src/configuration';
-export {default as MultiMap}        from './src/multiMap';
-export *                            from './src/format/date';
-export {default as NetAuthorizationV2Builder } from './src/net/authV2';
-export {default as NetEnvironment}  from './src/net/environment';
-export {default as NetHttpHeaders,
-    HttpMethod as NetHttpMethod }  from './src/net/httpHeaders';
-export {default as NetNodeUrlHelperMixin }  from './src/net/nodeUrlHelperMixin';
-export {default as NetUrlHelper}    from './src/net/urlHelper';
-export {default as netUrlQuery}     from './src/net/urlQuery';
+export { default as Configuration }   from './src/configuration';
+export { default as MultiMap }        from './src/multiMap';
+export { default as Pagination }      from './src/pagination';
+export { default as SortDescriptor }  from './src/sortDescriptor';
+export *                              from './src/format/date';
+export { default as NetAuthorizationV2Builder } from './src/net/authV2';
+export { default as NetEnvironment }  from './src/net/environment';
+export { default as NetHttpHeaders,
+    HttpMethod as NetHttpMethod } from './src/net/httpHeaders';
+export { default as NetNodeUrlHelperMixin }  from './src/net/nodeUrlHelperMixin';
+export { default as NetUrlHelper }    from './src/net/urlHelper';
+export { default as netUrlQuery }     from './src/net/urlQuery';
--- solarnetwork-query/index.js
+++ solarnetwork-query/index.js
@@ -1,10 +1,8 @@
 export { default as Aggregation } from './src/aggregation';
-export { default as Pagination } from './src/pagination';
 export { default as QueryUrlHelperMixin,
 	DefaultSolarQueryPath,
 	SolarQueryPathKey,
 	SolarQueryApiPathV1,
 	SolarQueryPublicPathKey } from './src/queryUrlHelperMixin';
-export { default as SortDescriptor } from './src/sortDescriptor';
 export { default as NodeDatumUrlHelperMixin,
 	NodeDatumUrlHelper } from './src/datum/nodeDatumUrlHelperMixin';
