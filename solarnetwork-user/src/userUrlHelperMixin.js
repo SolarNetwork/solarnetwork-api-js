f31cd4ed1c5a0d1143df54d610fd7c21266195c1
Matt Magoffin <git+matt@msqr.us>
2017-07-13 15:28:34 +1200

    Add /pending and /archived node URL support.

--- solarnetwork-user/src/userUrlHelperMixin.js
+++ solarnetwork-user/src/userUrlHelperMixin.js
@@ -5,7 +5,7 @@ export const SolarUserPathKey = 'solarUserPath';
 export const SolarUserApiPathV1 = '/api/v1/sec';
 
 /**
- * A mixin class that adds SolarUser specific support to {@code UrlHelper}.
+ * A mixin class that adds SolarUser specific support to <code>UrlHelper}.
  * 
  * @param {UrlHelper} superclass the UrlHelper class to mix onto 
  * @preserve
@@ -16,9 +16,9 @@ const UserUrlHelperMixin = (superclass) => class extends superclass {
 	 * Get the base URL to the SolarUser v1 REST API.
 	 * 
 	 * The returned URL uses the configured environment to resolve
-	 * the {@code hostUrl} and a {@code solarUserPath} context path.
+	 * the <code>hostUrl</code> and a <code>solarUserPath</code> context path.
 	 * If the context path is not available, it will default to 
-	 * {@code /solaruser}.
+	 * <code>/solaruser</code>.
 	 * 
 	 * @returns {string} the base URL to SolarUser
 	 * @preserve
@@ -29,13 +29,51 @@ const UserUrlHelperMixin = (superclass) => class extends superclass {
 	}
 
 	/**
-	 * Generate a SolarUser {@code /nodes} URL.
+	 * Generate a URL to get a list of all active nodes for the user account.
 	 *
-	 * @return {string} the URL to access the active user's nodes
+	 * @return {string} the URL to access the user's active nodes
 	 * @preserve
 	 */
 	viewNodesUrl() {
 		return this.baseUrl() + '/nodes';
+	}
+
+	/**
+	 * Generate a URL to get a list of all pending nodes for the user account.
+	 *
+	 * @return {string} the URL to access the user's pending nodes
+	 * @preserve
+	 */
+	viewPendingNodesUrl() {
+		return this.baseUrl() + '/nodes/pending';
+	}
+
+	/**
+	 * Generate a URL to get a list of all archived nodes for the user account.
+	 *
+	 * @return {string} the URL to access the user's archived nodes
+	 * @preserve
+	 */
+	viewArchivedNodesUrl() {
+		return this.baseUrl() + '/nodes/archived';
+	}
+
+	/**
+	 * Generate a URL to update the archived status of a set of nodes via a <code>POST</code> request.
+	 *
+	 * @param {number|number[]} nodeId a specific node ID, or array of node IDs, to update; if not provided the 
+	 *                                   <code>nodeIds</code> property of this class will be used
+	 * @param {boolean} archived <code>true</code> to mark the nodes as archived; <code>false</code> to un-mark
+	 *                           and return to normal status
+	 * @return {string} the URL to update the nodes archived status
+	 * @preserve
+	 */
+	updateNodeArchivedStatusUrl(nodeId, archived) {
+		const nodeIds = Array.isArray(nodeId) ? nodeId : nodeId ? [nodeId] : this.nodeIds;
+		let result = this.baseUrl() + '/nodes/archived?nodeIds='
+			+nodeIds.join(',') +'&archived='
+			+(archived ? 'true' : 'false');
+		return result;
 	}
 
 };
--- solarnetwork-user/test/userUrlHelperMixinTests.js
+++ solarnetwork-user/test/userUrlHelperMixinTests.js
@@ -25,6 +25,16 @@ test('user:userUrlHelperMixin:viewNodesUrl', t => {
 	t.is(helper.viewNodesUrl(), 'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes');
 });
 
+test('user:userUrlHelperMixin:viewPendingNodesUrl', t => {
+	const helper = new UserUrlHelper();
+	t.is(helper.viewPendingNodesUrl(), 'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/pending');
+});
+
+test('user:userUrlHelperMixin:viewArchivedNodesUrl', t => {
+	const helper = new UserUrlHelper();
+	t.is(helper.viewArchivedNodesUrl(), 'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/archived');
+});
+
 class UserNodeUrlHelper extends UserUrlHelperMixin(NodeUrlHelperMixin(UrlHelper)) {
 	
 }
@@ -38,4 +48,21 @@ test('user:userNodeUrlHelperMixin:nodeId', t => {
 	const helper = new UserNodeUrlHelper();
 	helper.nodeId = 123;
 	t.deepEqual(helper.nodeIds, [123]);
+});
+
+test('user:userUrlHelperMixin:updateNodeArchivedStatusUrl', t => {
+	const helper = new UserNodeUrlHelper();
+	helper.nodeId = 123;
+	t.is(helper.updateNodeArchivedStatusUrl(), 
+		'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/archived?'
+		+'nodeIds=123&archived=false');
+	t.is(helper.updateNodeArchivedStatusUrl(234), 
+		'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/archived?'
+		+'nodeIds=234&archived=false');
+	t.is(helper.updateNodeArchivedStatusUrl([234, 456]), 
+		'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/archived?'
+		+'nodeIds=234,456&archived=false');
+	t.is(helper.updateNodeArchivedStatusUrl(234, true), 
+		'https://data.solarnetwork.net/solaruser/api/v1/sec/nodes/archived?'
+		+'nodeIds=234&archived=true');
 });
