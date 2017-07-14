481b24c8220570589af10eefbc5872a32e8c28c7
Matt Magoffin <matt@solarnetwork.net>
2017-07-14 20:58:35 +1200

    Add new classes.

--- solarnetwork-core/index.js
+++ solarnetwork-core/index.js
@@ -1,9 +1,13 @@
-export { default as Aggregation }     from './src/aggregation';
-export { default as Configuration }   from './src/configuration';
-export { default as MultiMap }        from './src/multiMap';
-export { default as Pagination }      from './src/pagination';
-export { default as SortDescriptor }  from './src/sortDescriptor';
-export *                              from './src/format/date';
+export { default as Aggregations, Aggregation } from './src/aggregation';
+export { default as Configuration } from './src/configuration';
+export { default as ComparableEnum } from './src/comparableEnum';
+export { default as Enum } from './src/enum';
+export { default as LocationPrecisions, LocationPrecision } from './src/locationPrecision';
+export { default as MultiMap } from './src/multiMap';
+export { default as Pagination } from './src/pagination';
+export { default as SecurityPolicy, SecurityPolicyBuilder } from 'securityPolicy';
+export { default as SortDescriptor } from './src/sortDescriptor';
+export *  from './src/format/date';
 export { default as NetAuthorizationV2Builder } from './src/net/authV2';
 export { default as NetEnvironment }  from './src/net/environment';
 export { default as NetHttpHeaders,
