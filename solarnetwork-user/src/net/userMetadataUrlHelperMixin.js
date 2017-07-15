df20f07391eee9b10591fcb6115866f3758deca6
Matt Magoffin <matt@solarnetwork.net>
2017-07-15 20:21:44 +1200

    Add viewUserMetadataUrl() helper.

--- solarnetwork-user/src/net/userMetadataUrlHelperMixin.js
+++ solarnetwork-user/src/net/userMetadataUrlHelperMixin.js
@@ -9,6 +9,31 @@ import UserUrlHelperMixin from 'net/userUrlHelperMixin'
  */
 const UserMetadataUrlHelperMixin = (superclass) => class extends superclass {
 
+    /**
+	 * Generate a URL for viewing the configured user's metadata via a <code>GET</code> request.
+	 *
+	 * @param {number|number[]|null} [userId] a specific user ID (or set of IDs);
+     *                                        if not provided the <code>userIds</code> property of this class will be used;
+     *                                        if <code>null</code> then get the metadata for the requesting user
+	 * @returns {string} the URL
+     * @memberof UserMetadataUrlHelperMixin#
+	 */
+	viewUserMetadataUrl(userId) {
+        let result = this.baseUrl() +'/users/meta';
+        let userIds = (userId || this.userIds);
+        if ( userIds && userId !== null ) {
+            if ( Array.isArray(userIds) ) {
+                if ( userIds.length > 1 ) {
+                    result += '?userIds=' +userIds.join(',');
+                } else {
+                    result += '?userId=' +userIds[0];
+                }
+            } else {
+                result += '?userId=' +userIds;
+            }
+        }
+        return result;
+	}
 }
 
 export default UserMetadataUrlHelperMixin;
--- solarnetwork-user/test/net/userMetadataUrlHelperMixinTests.js
+++ solarnetwork-user/test/net/userMetadataUrlHelperMixinTests.js
@@ -6,3 +6,43 @@ test('user:net:userUrlHelperMixin:create', t => {
 	const helper = new UserMetadataUrlHelper();
 	t.truthy(helper);
 });
+
+test('user:net:viewUserMetadataUrl:noUser', t => {
+	const helper = new UserMetadataUrlHelper();
+	const result = helper.viewUserMetadataUrl();
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta');
+});
+
+test('user:net:viewUserMetadataUrl:helperUser', t => {
+	const helper = new UserMetadataUrlHelper();
+	helper.userId = 123;
+	const result = helper.viewUserMetadataUrl();
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta?userId=123');
+});
+
+test('user:net:viewUserMetadataUrl:helperUsers', t => {
+	const helper = new UserMetadataUrlHelper();
+	helper.userIds = [123, 234];
+	const result = helper.viewUserMetadataUrl();
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta?userIds=123,234');
+});
+
+test('user:net:viewUserMetadataUrl:argOverridesHelperUsers', t => {
+	const helper = new UserMetadataUrlHelper();
+	helper.userId = 123;
+	const result = helper.viewUserMetadataUrl(234);
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta?userId=234');
+});
+
+test('user:net:viewUserMetadataUrl:argUsers', t => {
+	const helper = new UserMetadataUrlHelper();
+	const result = helper.viewUserMetadataUrl([123, 234]);
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta?userIds=123,234');
+});
+
+test('user:net:viewUserMetadataUrl:argUsers', t => {
+	const helper = new UserMetadataUrlHelper();
+	helper.userId = 123;
+	const result = helper.viewUserMetadataUrl(null);
+	t.is(result, 'https://data.solarnetwork.net/solaruser/api/v1/sec/users/meta');
+});
